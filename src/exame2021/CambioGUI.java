dbf32e6203f3159f80f9af7b213a32fc382ee695
SheltonTeles <104696952+SheltonTeles@users.noreply.github.com>
2023-11-14 16:59:34 +0200

    Exame 2021 - tela de conversão

--- src/exame2021/CambioGUI.java
+++ src/exame2021/CambioGUI.java
@@ -9,13 +9,24 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+/*Resolução da tela de conversão do exame de programação 3, 2021 ISUTC
+ * 
+ * A tela abaixo serve para converter um valor em moeda estrangeira para metical. 
+ * Ao selecionar a moeda o respectivo câmbio deve ser preenchido;
+ * Os campos do Valor em MT e Câmbio não podem ser editáveis
+ * O botão converter serve para calcular e ilustrar o valor em meticais de acordo com
+ * a taxa de câmbio da  moeda selecionada
+ * 
+ * O botão histórico foi um acréscimo pedido pelo docente para podermos implementar a base de
+ * dados*/
 public class CambioGUI extends JFrame implements ActionListener{
-	public static final double usd=64.46 , eur=72.67, zar =4.09;//taxas de câmbio
+	public static final double usd=64.46 , eur=72.67, zar =4.09;
+	public static final String usdString="64.46" , eurString="72.67", zarString ="4.09";//taxas de câmbio
+	public static double valorInserido, valorConvertido;
 
 	private JLabel labelValorMoeda, labelMoeda, labelValorMT, labelCambio;
 	private JTextField tf_valorMoeda, tf_moeda, tf_cambio;
-	private JButton bt_converter;
+	private JButton bt_converter, ver_historico;
 	private JComboBox optionsMoedas;
 	private JPanel panel1;
 
@@ -40,25 +51,28 @@ public class CambioGUI extends JFrame implements ActionListener{
 
 		labelValorMT = new JLabel("Valor em MT",0);
 		tf_moeda = new JTextField(20);
+		tf_moeda.setEditable(false);
 
 		labelCambio = new JLabel("Câmbio",0);
 		tf_cambio = new JTextField(8);
 		tf_cambio.setEditable(false);
-		
+
 		this.add(panel1);
 		panel1.add(labelValorMoeda);
 		panel1.add(tf_valorMoeda);
 		panel1.add(labelMoeda);
 		panel1.add(optionsMoedas);
-		
+
 		panel1.add(labelValorMT);
 		panel1.add(tf_moeda);
 		panel1.add(labelCambio);
 		panel1.add(tf_cambio);
 
 		bt_converter = new JButton("Converter");
+		ver_historico = new JButton("Historico");
 		bt_converter.addActionListener(this);
 		this.add(bt_converter);
+		this.add(ver_historico);
 
 		this.setVisible(true);
 
@@ -68,14 +82,44 @@ public class CambioGUI extends JFrame implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
-		if(e.getSource() == optionsMoedas) {
-			
-			
+
+		//		if(e.getSource() == optionsMoedas) {
+		//			
+		//			
+		//		}
+
+		valorInserido = Double.parseDouble(tf_valorMoeda.getText());
+
+
+
+		switch(optionsMoedas.getSelectedItem().toString()) {
+		case "USD":
+			tf_cambio.setText(usdString);
+			valorConvertido = valorInserido*usd;
+			break;
+
+		case "EUR":
+			tf_cambio.setText(eurString);
+			valorConvertido = valorInserido*eur;
+			break;
+
+		case "ZAR":
+			tf_cambio.setText(zarString);
+			valorConvertido = valorInserido*zar;
+			break;
 		}
-		
+
+
+
 		if(e.getSource() == bt_converter) {
+
+			tf_moeda.setText(valorConvertido+"MT");
+
+		}
+
+		if(e.getSource() == ver_historico) {
 			
+//			new historico();
 		}
 
 	}
